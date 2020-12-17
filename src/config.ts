87f8acd8a676786d488efbb8307d1a7c7e184bc6
Arnei <arnepokemon@yahoo.de>
2020-12-17 11:37:27 +0100

    Changed mediaPackageId

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@
 /**
  * Hardcoded id of the mediapackge the editor will work on
  */
-export const mediaPackageId : string = "0ca476fa-8cf0-4061-b6bf-70ea1b365d64"
+export const mediaPackageId : string = "e63706bc-48df-4346-a72a-bf6f39cea32c"
