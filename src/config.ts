d9bb542811675a3aa8f098deff896c2015421c60
Arnei <arnepokemon@yahoo.de>
2020-12-18 11:44:22 +0100

    experimental bs

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,34 @@
 /**
  * A place for global config values
  */
+import TOML from '@iarna/toml';
+// var fs = require('fs')
+import fs from 'fs'
+
 
 /**
  * Hardcoded id of the mediapackge the editor will work on
  */
-export const mediaPackageId : string = "e63706bc-48df-4346-a72a-bf6f39cea32c"
+
+
+export var mediaPackageId : string = "e63706bc-48df-4346-a72a-bf6f39cea32c"
+var urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has("mediaPackageId")) {
+  let tmp = urlParams.get("mediaPackageId")
+  if (tmp) {
+    mediaPackageId = tmp
+  }
+}
+
+ var test = parse("./editor-settings.toml")
+
+function parse(configPath: string) {
+  if (!fs.existsSync(configPath)) {
+   throw new Error(`config file not found: ${configPath}`);
+  }
+  try {
+   return TOML.parse(fs.readFileSync(configPath));
+  } catch (err) {
+   throw new Error(`config file parse failed: ${configPath}`);
+  }
+ }
