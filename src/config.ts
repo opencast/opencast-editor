39bf802a7383cec67c31333843eb9dd36584ba8f
Arnei <arnepokemon@yahoo.de>
2020-12-18 11:48:51 +0100

    Allows passing a mediaPackageId for testing

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,11 @@
 /**
  * Hardcoded id of the mediapackge the editor will work on
  */
-export const mediaPackageId : string = "e63706bc-48df-4346-a72a-bf6f39cea32c"
+export var mediaPackageId : string = "e63706bc-48df-4346-a72a-bf6f39cea32c"
+var urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has("mediaPackageId")) {
+  let tmp = urlParams.get("mediaPackageId")
+  if (tmp) {
+    mediaPackageId = tmp
+  }
+}
