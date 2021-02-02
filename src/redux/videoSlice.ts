opencast/opencast-editor

commit 7b501e0a55620c9241fb72a3be0874888eeecfd5

Introducing unit tests for redux slices

src/redux/videoSlice.ts
@@ -25,7 +25,7 @@ export interface video {
   workflows: Workflow[],
 }
 
-const initialState: video & httpRequestState = {
+export const initialState: video & httpRequestState = {
   isPlaying: false,
   isPlayPreview: true,
   currentlyAt: 0,   // Position in the video in milliseconds
@@ -77,14 +77,19 @@ export const videoSlice = createSlice({
     setCurrentlyAt: (state, action: PayloadAction<video["currentlyAt"]>) => {
       state.currentlyAt = roundToDecimalPlace(action.payload, 0);
 
+      if (state.currentlyAt < 0) {
+        state.currentlyAt = 0;
+      }
+
+      if (state.duration !== 0 && state.duration < state.currentlyAt) {
+        state.currentlyAt = state.duration
+      }
+
       updateActiveSegment(state);
       skipDeletedSegments(state);
     },
     setCurrentlyAtInSeconds: (state, action: PayloadAction<video["currentlyAt"]>) => {
-      state.currentlyAt = roundToDecimalPlace(action.payload * 1000, 0);
-
-      updateActiveSegment(state);
-      skipDeletedSegments(state);
+      setCurrentlyAt(roundToDecimalPlace(action.payload * 1000, 0))
     },
     addSegment: (state, action: PayloadAction<video["segments"][0]>) => {
       state.segments.push(action.payload)
@@ -190,7 +195,7 @@ const updateActiveSegment = (state: WritableDraft<video>) => {
 /**
  * Helper Function for testing with current/old editor API
  */
-const parseSegments = (segments: Segment[], duration: number) => {
+export const parseSegments = (segments: Segment[], duration: number) => {
   let newSegments : Segment[] = []
 
   if (segments.length === 0) {

src/App.test.tsx
@@ -1,9 +1,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});

src/redux/__tests__/videoSlice.test.ts
@@ -1,0 +1,355 @@
+import reducer, { initialState, setIsPlaying, selectIsPlaying, setCurrentlyAt,
+  selectCurrentlyAt, selectActiveSegmentIndex, selectPreviewTriggered,
+  selectDuration, video, cut, selectSegments, markAsDeletedOrAlive, mergeRight,
+  fetchVideoInformation, selectVideoURL, selectTitle, selectPresenters,
+  selectTracks, selectWorkflows } from '../videoSlice'
+import cloneDeep from 'lodash/cloneDeep';
+import { httpRequestState } from '../../types';
+
+
+describe('Video reducer', () => {
+
+  let initState: (video & httpRequestState);
+
+  beforeEach(() => {
+    initState = cloneDeep(initialState);
+  });
+
+  it('should return the initial state on first run', () => {
+    // Arrange
+    const nextState = initialState;
+
+    // Act
+    const result = reducer(undefined, { type: ''});
+
+    // Assert
+    expect(result).toEqual(nextState);
+  });
+
+  it('should set isPlaying', () => {
+    // Arrange
+    const isPlaying = true
+
+    // Act
+    const nextState = reducer(initState, setIsPlaying(isPlaying))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectIsPlaying(rootState)).toEqual(true);
+  })
+
+  it('should set currentlyAt', () => {
+    // Arrange
+    const currentlyAt = 0
+
+    // Act
+    const nextState = reducer(initState, setCurrentlyAt(currentlyAt))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectCurrentlyAt(rootState)).toEqual(0);
+    expect(selectActiveSegmentIndex(rootState)).toEqual(0);
+    expect(selectPreviewTriggered(rootState)).toEqual(false);
+  })
+
+  it('only allow whole numbers for currentlyAt', () => {
+    // Arrange
+    const currentlyAt = 123.456
+
+    // Act
+    const nextState = reducer(initState, setCurrentlyAt(currentlyAt))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectCurrentlyAt(rootState)).toEqual(123);
+    expect(selectActiveSegmentIndex(rootState)).toEqual(0);
+    expect(selectPreviewTriggered(rootState)).toEqual(false);
+  })
+
+  it('should not set currentlyAt to negative', () => {
+    // Arrange
+    const currentlyAt = -42
+
+    // Act
+    const nextState = reducer(initState, setCurrentlyAt(currentlyAt))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectCurrentlyAt(rootState)).toEqual(0);
+    expect(selectActiveSegmentIndex(rootState)).toEqual(0);
+    expect(selectPreviewTriggered(rootState)).toEqual(false);
+  })
+
+  it('should not set currentlyAt larger than duration', () => {
+    // Arrange
+    const currentlyAt = 43
+    initState.duration = 42
+
+    // Act
+    const nextState = reducer(initState, setCurrentlyAt(currentlyAt))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectCurrentlyAt(rootState)).toEqual(42);
+    expect(selectActiveSegmentIndex(rootState)).toEqual(0);
+    expect(selectPreviewTriggered(rootState)).toEqual(false);
+  })
+
+  it('should change the activeSegment when setting currentlyAt', () => {
+    // Arrange
+    const currentlyAt = 15
+    initState.duration = 30
+    initState.segments = [
+      {id: '0', start: 0, end: 10, deleted: false},
+      {id: '0', start: 10, end: 20, deleted: false},
+      {id: '0', start: 20, end: 30, deleted: false},
+    ]
+
+    // Act
+    const nextState = reducer(initState, setCurrentlyAt(currentlyAt))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectCurrentlyAt(rootState)).toEqual(15);
+    expect(selectActiveSegmentIndex(rootState)).toEqual(1);
+    expect(selectPreviewTriggered(rootState)).toEqual(false);
+  })
+
+  it(`should set currentlyAt to end of segment when preview is active, the video
+      is playing and segment is deleted`, () => {
+    // Arrange
+    const currentlyAt = 15
+    initState.duration = 30
+    initState.segments = [
+      {id: '0', start: 0, end: 10, deleted: false},
+      {id: '0', start: 10, end: 20, deleted: true},
+      {id: '0', start: 20, end: 30, deleted: false},
+    ]
+    initState.isPlaying = true
+    initState.isPlayPreview = true
+
+    // Act
+    const nextState = reducer(initState, setCurrentlyAt(currentlyAt))
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectCurrentlyAt(rootState)).toEqual(20);
+    expect(selectActiveSegmentIndex(rootState)).toEqual(1);
+    expect(selectPreviewTriggered(rootState)).toEqual(true);
+  })
+
+  it('should cut a segment in two', () => {
+    // Arrange
+    initState.currentlyAt = 5
+    initState.duration = 10
+    initState.segments = [
+      {id: '0', start: 0, end: 10, deleted: false},
+    ]
+    let resultSegments = [
+      {start: 0, end: 5, deleted: false},
+      {start: 5, end: 10, deleted: false}
+    ]
+
+    // Act
+    const nextState = reducer(initState, cut())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should not cut a segment when exactly at the start', () => {
+    // Arrange
+    initState.currentlyAt = 0
+    initState.duration = 10
+    initState.segments = [
+      {id: '0', start: 0, end: 10, deleted: false},
+    ]
+    let resultSegments = [
+      {start: 0, end: 10, deleted: false},
+    ]
+
+    // Act
+    const nextState = reducer(initState, cut())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should not cut a segment when exactly at the end', () => {
+    // Arrange
+    initState.currentlyAt = 10
+    initState.duration = 10
+    initState.segments = [
+      {id: '0', start: 0, end: 10, deleted: false},
+    ]
+    let resultSegments = [
+      {start: 0, end: 10, deleted: false},
+    ]
+
+    // Act
+    const nextState = reducer(initState, cut())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should not cut a segment when exactly at the border', () => {
+    // Arrange
+    initState.currentlyAt = 5
+    initState.duration = 10
+    initState.segments = [
+      {id: '0', start: 0, end: 5, deleted: false},
+      {id: '0', start: 5, end: 10, deleted: false}
+    ]
+    let resultSegments = [
+      {start: 0, end: 5, deleted: false},
+      {start: 5, end: 10, deleted: false}
+    ]
+
+    // Act
+    const nextState = reducer(initState, cut())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should mark a segment as deleted if alive', () => {
+    let resultSegments = [
+      {deleted: true},
+    ]
+
+    // Act
+    const nextState = reducer(initState, markAsDeletedOrAlive())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should merge segments if possible', () => {
+    initState.segments = [
+      {id: '0', start: 0, end: 5, deleted: false},
+      {id: '0', start: 5, end: 10, deleted: false}
+    ]
+    let resultSegments = [
+      {start: 0, end: 10, deleted: false},
+    ]
+
+    // Act
+    const nextState = reducer(initState, mergeRight())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should not merge segments if not possible', () => {
+    initState.activeSegmentIndex = 1
+    initState.segments = [
+      {id: '0', start: 0, end: 5, deleted: false},
+      {id: '0', start: 5, end: 10, deleted: false}
+    ]
+    let resultSegments = [
+      {id: '0', start: 0, end: 5, deleted: false},
+      {id: '0', start: 5, end: 10, deleted: false}
+    ]
+
+    // Act
+    const nextState = reducer(initState, mergeRight())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should keep state of active segment when merging', () => {
+    initState.segments = [
+      {id: '0', start: 0, end: 5, deleted: true},
+      {id: '0', start: 5, end: 10, deleted: false}
+    ]
+    let resultSegments = [
+      {start: 0, end: 10, deleted: true},
+    ]
+
+    // Act
+    const nextState = reducer(initState, mergeRight())
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(selectSegments(rootState)).toMatchObject(resultSegments);
+  })
+
+  it('should set loading when fetch is pending', () => {
+    // Arrange
+    const action = { type: fetchVideoInformation.pending.type };
+    const resultStatus: httpRequestState = { status: 'loading', error: undefined }
+
+    // Act
+    const nextState = reducer(initialState, action);
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(rootState.videoState).toMatchObject(resultStatus);
+  })
+
+  it('should set success when fetch is successful', () => {
+    // Arrange
+    const resultStatus: httpRequestState = { status: 'success', error: undefined }
+    const segments = [{ start: 0, end: 42, deleted: false }]
+    const videoURLs: video["videoURLs"] = [ "video/url" ]
+    const dur: video["duration"] = 42
+    const title: video["title"] = "Video Title"
+    // const presenters: video["presenters"] = [ "Otto Opencast" ]    // Currently missing from the API
+    const tracks: video["tracks"] = [{
+      id: "id", uri: videoURLs[0], flavor: { subtype: "prepared", type: "presenter"},
+      videoStream: { available: true, enabled: true, thumbnail_uri: "thumb/url"},
+      audioStream: { available: true, enabled: true}
+    }]
+    const workflows: video["workflows"] = [{ id: "id", name: "Name", displayOrder: 0, description: "Description"}]
+    const action = {
+      type: fetchVideoInformation.fulfilled.type,
+      payload: {
+        duration: dur,
+        title: title,
+        workflows: workflows,
+        tracks: tracks,
+        segments: segments
+      }
+    };
+
+    // Act
+    const nextState = reducer(initialState, action);
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(rootState.videoState).toMatchObject(resultStatus);
+
+    expect(selectSegments(rootState)).toMatchObject(segments);
+    expect(selectVideoURL(rootState)).toMatchObject(videoURLs);
+    expect(selectDuration(rootState)).toEqual(dur);
+    expect(selectTitle(rootState)).toEqual(title);
+    expect(selectPresenters(rootState)).toEqual([]);
+    expect(selectTracks(rootState)).toMatchObject(tracks);
+    expect(selectWorkflows(rootState)).toMatchObject(workflows);
+  })
+
+  it('should set error when fetch is failed', () => {
+    // Arrange
+    const error = { message: "An error message" }
+    const action = { type: fetchVideoInformation.rejected.type, error: error };
+    const resultStatus: httpRequestState = { status: 'failed', error: error.message }
+
+    // Act
+    const nextState = reducer(initialState, action);
+
+    // Assert
+    const rootState = { videoState: nextState };
+    expect(rootState.videoState).toMatchObject(resultStatus);
+  })
+
+  // TODO: Figure out how to properly mock the API and the redux store to test requests
+})

commit 3bc9ebfb27d9dd5b804e4b0afd1da0c21273ee2e

Fix "`currentlyAt` not updating" bug introduced in 7b501e0
The problem was that it was attempted to call another reducer, but
with wrong arguments. Now the code shared by both reducers have been
put into another function.

src/redux/videoSlice.ts
@@ -57,6 +57,21 @@ export const fetchVideoInformation = createAsyncThunk('video/fetchVideoInformati
   return response
 })
 
+const updateCurrentlyAt = (state: video, milliseconds: number) => {
+  state.currentlyAt = roundToDecimalPlace(milliseconds, 0);
+
+  if (state.currentlyAt < 0) {
+    state.currentlyAt = 0;
+  }
+
+  if (state.duration !== 0 && state.duration < state.currentlyAt) {
+    state.currentlyAt = state.duration
+  }
+
+  updateActiveSegment(state);
+  skipDeletedSegments(state);
+};
+
 /**
  * Slice for the state of the "video"
  * Treats the multitude of videos that may exist as one video
@@ -75,21 +90,10 @@ export const videoSlice = createSlice({
       state.previewTriggered = action.payload
     },
     setCurrentlyAt: (state, action: PayloadAction<video["currentlyAt"]>) => {
-      state.currentlyAt = roundToDecimalPlace(action.payload, 0);
-
-      if (state.currentlyAt < 0) {
-        state.currentlyAt = 0;
-      }
-
-      if (state.duration !== 0 && state.duration < state.currentlyAt) {
-        state.currentlyAt = state.duration
-      }
-
-      updateActiveSegment(state);
-      skipDeletedSegments(state);
+      updateCurrentlyAt(state, action.payload);
     },
     setCurrentlyAtInSeconds: (state, action: PayloadAction<video["currentlyAt"]>) => {
-      setCurrentlyAt(roundToDecimalPlace(action.payload * 1000, 0))
+      updateCurrentlyAt(state, roundToDecimalPlace(action.payload * 1000, 0))
     },
     addSegment: (state, action: PayloadAction<video["segments"][0]>) => {
       state.segments.push(action.payload)