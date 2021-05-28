614c3df1a0729628bff82bcc1e907384310dc4eb
Arnei <arnewilken@yahoo.de>
2021-05-28 09:42:23 +0200

    Reduced wordiness of errorBox

--- src/main/ErrorBox.tsx
+++ src/main/ErrorBox.tsx
@@ -17,26 +17,21 @@ import { httpRequestState } from '../types'
   const errorCodeMessages = (status: number) => {
     switch (status) {
       case 423:
-        return "Code 423. There is a workflow running in Opencast. You have to wait until it finishes before you can perform that action."
+        return "There is a workflow running in Opencast. You have to wait until it finishes before you can perform that action. "
       case 500:
-        return "Code 500. Something went seriously wrong in Opencast."
+        return "Something went seriously wrong in Opencast. "
       default:
-        return "Unknown error code " + status + "."
+        return "Unknown error code " + status + ". "
     }
   }
-
-  const centerChildstyle = () => css({
-    alignSelf: 'center',
-  })
 
   const errorBoxStyle = (errorStatus: boolean) => {
     return (
       css({
-        display: errorStatus ? "flex" : "none",
-        flexDirection: 'column',
+        ...(!errorStatus) && {display: "none"},
         borderColor: 'red',
         borderStyle: 'dashed',
-        fontWeight: 'bold',
+        fontStyle: 'italic',
         padding: '10px',
       })
     );
@@ -44,12 +39,14 @@ import { httpRequestState } from '../types'
 
   return (
     <div css={errorBoxStyle(showBox)} role="alert">
-      <span css={centerChildstyle}>{"An error occured!"}</span>
-      <span>{errorMessage}</span>
-      <span css={centerChildstyle}>{errorDetails ? t("various.error-details-text") : t("various.error-noDetails-text")}</span>
+      <span css={{fontWeight: 'bold'}}>{t("various.error-header-text")}</span>
+      <span>{errorMessage.trim() + " "}</span>
+      <span>{errorDetails ? "" : t("various.error-noDetails-text")}</span>
       <span>{(errorDetails && typeof errorDetails === 'string') ? errorDetails : ""}</span>
-      <span>{(errorDetails && typeof errorDetails !== 'string' && "status" in errorDetails) ? errorCodeMessages(errorDetails.status) : ""}</span>
-      <span>{(errorDetails && typeof errorDetails !== 'string' && "status" in errorDetails) ? errorDetails.text : ""}</span>
+      <span>{(errorDetails && typeof errorDetails !== 'string' && "status" in errorDetails) ?
+        errorCodeMessages(errorDetails.status) : ""}</span>
+      <span>{(errorDetails && typeof errorDetails !== 'string' && "text" in errorDetails && errorDetails.text) ?
+        t("various.error-details-text", {errorMessage: errorDetails.text}) : ""}</span>
   </div>
   );
 }
