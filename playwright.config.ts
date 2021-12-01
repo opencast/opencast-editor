25fd5fa6fcbedf2234ed71c4b59d1bfd1d139c78
naweidner <weidner@elan-ev.de>
2021-12-01 18:30:29 +0100

    Worker added
    Added number of maximum workers in playwright.config. Workflow was only running with one worker.

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ const config: PlaywrightTestConfig = {
   reporter: [ ['html', { outputFolder: 'playwright-report' }]],
   testIgnore: '**/redux/**',
   retries: 3,
+  workers: 4,
 
   use: {
     baseURL: 'http://localhost:3000/',
