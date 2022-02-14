94ff3ec8b278331b6c02dacb26275737d141ffb5
Arnei <arnewilken@yahoo.de>
2022-02-14 12:23:04 +0100

    Basic Substitle segment List View
    First implementation of a list view for subtitle editing. Does not
    contain any functionality yet.

--- src/main/SubtitleEditor.tsx
+++ src/main/SubtitleEditor.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { css } from "@emotion/react";
 import Timeline from "./Timeline";
 import ReactPlayer from "react-player";
-import { basicButtonStyle } from "../cssStyles";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { basicButtonStyle, flexGapReplacementStyle } from "../cssStyles";
+import { faArrowDown, faChevronLeft, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /**
- * Displays a menu for selecting what should be done with the current changes
+ * Displays an editor view for a selected subtitle file
  */
  const SubtitleEditor : React.FC<{displayEditView: (e: boolean) => void}> = (displayEditView) => {
 
@@ -61,7 +61,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
         <div css={[titleStyle, titleStyleBold]}>
           Subtitle Editor - [Language Name]
         </div>
-        <div css={{widht: '50px'}}></div>
+        <div css={{width: '50px'}}></div>
       </div>
       <div css={subAreaStyle}>
         <SubtitleListEditor />
@@ -72,17 +72,167 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
   );
 }
 
+/**
+ * Displays everything needed to edit subtitles
+ */
 const SubtitleListEditor : React.FC<{}> = () => {
 
+  const dummyData : [string, string, string][] = [
+    ["", "", ""],
+    ["Bla", "00:00:00.000", "00:00:03.000"],
+    ["Fischers Frizt fischt frische Fische. Frische Fische fischt Fischers Fritz!", "00:00:05.000", "00:00:07.000"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]
+
   const listStyle = css({
-    backgroundColor: 'red',
+    display: 'flex',
+    flexDirection: 'column',
     height: '100%',
     width: '60%',
+    ...(flexGapReplacementStyle(20, false)),
   })
+
+  const headerStyle = css({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    flexWrap: 'wrap',
+    ...(flexGapReplacementStyle(20, false)),
+    paddingRight: '20px',
+  })
+
+  const segmentListStyle = css({
+    display: 'flex',
+    flexDirection: 'column',
+    ...(flexGapReplacementStyle(20, false)),
+    paddingTop: '2px',  // Else the select highlighting gets cut off
+    paddingBottom: '2px',
+    paddingRight: '10px',
+    overflowY: 'auto',
+  })
+
+  const cuttingActionButtonStyle = {
+    padding: '16px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+  };
 
   return (
     <div css={listStyle}>
-      List View
+      <div css={headerStyle}>
+        <div css={[basicButtonStyle, cuttingActionButtonStyle]}>Herunterladen</div>
+        <div css={[basicButtonStyle, cuttingActionButtonStyle]}>Hochladen</div>
+        <div css={[basicButtonStyle, cuttingActionButtonStyle]}>Alles löschen</div>
+      </div>
+      <div css={segmentListStyle}>
+        {dummyData.map((item, i) => {
+          return (
+            <SubtitleListSegment textInit={item[0]} startInit={item[1]} endInit={item[2]} key={i}/>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+/**
+ * A single subtitle segment
+ */
+const SubtitleListSegment : React.FC<{textInit: string, startInit: string, endInit: string}> = ({textInit, startInit, endInit}) => {
+
+  const [text, setText] = useState(textInit)
+  const [start, setStart] = useState(startInit)
+  const [end, setEnd] = useState(endInit)
+
+  const segmentStyle = css({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: '100px',
+    ...(flexGapReplacementStyle(20, false)),
+  })
+
+  const timeAreaStyle = css({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  })
+
+  // const functionButtonAreaStyle = css({
+  //   display: 'grid',
+  //   gridTemplateRows: '[row1-start] 20% [row1-end] 50% [third-line] 20% [last-line]',
+  //   // gridTemplateRows: '[row1-start] 50% [row1-end] 50% [last-line]',
+  //   gridTemplateColumns: 'repeat(2, 50%)',
+  //   placeitems: 'center',
+  //   gridGap: '10px',
+  //   flexGrow: '1',
+  //   minWidth: '50px',
+  // })
+
+  const functionButtonAreaStyle = css({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    ...(flexGapReplacementStyle(10, false)),
+    flexGrow: '1',
+    minWidth: '50px',
+  })
+
+  const fieldStyle = css({
+    fontSize: '1em',
+    marginLeft: '15px',
+    borderRadius: '5px',
+    boxShadow: '0 0 1px rgba(0, 0, 0, 0.3)',
+    padding: '10px 10px',
+    background: 'snow',
+  })
+
+  const textFieldStyle = css({
+    flexGrow: '7',
+    minWidth: '100px',
+    // TODO: Find a way to allow resizing without breaking the UI
+    //  Manual or automatic resizing can cause neighboring textareas to overlap
+    //  Can use TextareaAutosize from mui, but that does not fix the overlap problem
+    resize: 'none',
+  })
+
+  const timeFieldStyle = css({
+    height: '20%',
+    width: '100px',
+  })
+
+  const addSegmentButtonStyle = css({
+    maxWidth: '35px',
+    maxHeight: '15px',
+    padding: '16px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+    // gridRow: 'row1-end',
+  })
+
+  return (
+    <div css={segmentStyle}>
+      <textarea css={[fieldStyle, textFieldStyle]} name={"test"} defaultValue={text}></textarea>
+      <div css={functionButtonAreaStyle}>
+        {/* <div css={[basicButtonStyle, addSegmentButtonStyle]}>
+          <FontAwesomeIcon icon={faArrowUp} size="1x" />
+          <FontAwesomeIcon icon={faPlus} size="1x" />
+        </div> */}
+        <div css={[basicButtonStyle, addSegmentButtonStyle]}>
+          <FontAwesomeIcon icon={faTrash} size="1x" />
+        </div>
+        <div css={[basicButtonStyle, addSegmentButtonStyle]}>
+          <FontAwesomeIcon icon={faArrowDown} size="1x" />
+          <FontAwesomeIcon icon={faPlus} size="1x" />
+        </div>
+      </div>
+      <div css={timeAreaStyle}>
+        <input css={[fieldStyle, timeFieldStyle]} id={"start"} type={"text"} value={start}></input>
+        <input css={[fieldStyle, timeFieldStyle]} id={"end"} type={"text"} value={end}></input>
+      </div>
+      {/* <input id={"end"} type={"text"} value={end} onInput={e => setEnd((e.target as HTMLInputElement).value)}></input> */}
     </div>
   );
 }
