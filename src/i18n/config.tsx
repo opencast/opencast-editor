4dba0f0977dc8ed68cb89410fae830004827d95e
Lars Kiesow <lkiesow@uos.de>
2022-03-10 23:52:10 +0100

    Make Translations Work
    This patch makes translations actually work instead of always
    automatically falling back to English.
    
    The patch also makes sure the list of languages gets updated
    automatically whenever the translation files are being updated.

--- src/i18n/config.tsx
+++ src/i18n/config.tsx
@@ -2,17 +2,25 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import enTranslations from './locales/en.json';
+import locales from './locales/locales.json';
 
-const resources = {
-  en: { translation: enTranslations }
-};
+const debug = Boolean(new URLSearchParams(window.location.search).get('debug'));
 
-i18n
-    .use(initReactI18next)
+const resources: any = {};
+for (const lang of locales) {
+  const code = lang.replace(/\..*$/, '');
+  import('./locales/' + lang)
+  .then(translations => {
+    resources[code] = { translation: translations };
+  })
+}
+
+console.warn(typeof resources);
+
+i18n.use(initReactI18next)
     .use(LanguageDetector)
     .init({
         resources,
         fallbackLng: 'en',
+        debug: debug,
   });
-  
