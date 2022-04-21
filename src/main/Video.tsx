5d5cba52afb32c312ec89448e30cbcf4161c655e
Arnei <arnewilken@yahoo.de>
2022-04-21 10:02:01 +0200

    Removed unused import

--- src/main/Video.tsx
+++ src/main/Video.tsx
@@ -5,7 +5,7 @@ import { css } from '@emotion/react'
 import { httpRequestState, MainMenuStateNames } from '../types'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause, faToggleOn, faToggleOff, faGear, faGears} from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faPause, faToggleOn, faToggleOff, faGears} from "@fortawesome/free-solid-svg-icons";
 
 import { useSelector, useDispatch } from 'react-redux';
 import {
