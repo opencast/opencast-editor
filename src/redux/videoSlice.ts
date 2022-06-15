063801b47b5b4b61ba0cddd8593ba707cee5846a
Arne Wilken <Arnei@users.noreply.github.com>
2022-06-15 11:38:46 +0200

    Re-design of workflow selection (#444)
    This patch overhauls the look and feel of the workflow selection in case users have to choose a workflow in the start processing step.
    
    Resolves #295
    Resolves #296
    Closes #445

--- src/redux/videoSlice.ts
+++ src/redux/videoSlice.ts
@@ -15,7 +15,7 @@ export interface video {
   segments: Segment[],
   tracks: Track[],
   activeSegmentIndex: number,     // Index of the segment that is currenlty hovered
-  selectedWorkflowIndex: number,  // Index of the currently selected workflow
+  selectedWorkflowId: string,     // Id of the currently selected workflow
   aspectRatios: {width: number, height: number}[],  // Aspect ratios of every video
   hasChanges: boolean             // Did user make changes in cutting view since last save
   waveformImages: string[]
@@ -35,7 +35,7 @@ export const initialState: video & httpRequestState = {
   segments: [{id: nanoid(), start: 0, end: 1, deleted: false}],
   tracks: [],
   activeSegmentIndex: 0,
-  selectedWorkflowIndex: 0,
+  selectedWorkflowId: "",
   previewTriggered: false,
   clickTriggered: false,
   aspectRatios: [],
@@ -151,8 +151,8 @@ export const videoSlice = createSlice({
       state.segments[state.activeSegmentIndex].deleted = !state.segments[state.activeSegmentIndex].deleted
       state.hasChanges = true
     },
-    setSelectedWorkflowIndex: (state, action: PayloadAction<video["selectedWorkflowIndex"]>) => {
-      state.selectedWorkflowIndex = action.payload
+    setSelectedWorkflowIndex: (state, action: PayloadAction<video["selectedWorkflowId"]>) => {
+      state.selectedWorkflowId = action.payload
     },
     mergeLeft: (state) => {
       mergeSegments(state, state.activeSegmentIndex, state.activeSegmentIndex - 1)
@@ -322,9 +322,9 @@ export const selectActiveSegmentIndex = (state: { videoState: { activeSegmentInd
 export const selectIsCurrentSegmentAlive = (state: { videoState:
   { segments: { [x: number]: { deleted: boolean; }; }; activeSegmentIndex: video["activeSegmentIndex"]; }; }) =>
   !state.videoState.segments[state.videoState.activeSegmentIndex].deleted
-export const selectSelectedWorkflowIndex = (state: { videoState:
-  { selectedWorkflowIndex: video["selectedWorkflowIndex"]; }; }) =>
-  state.videoState.selectedWorkflowIndex
+export const selectSelectedWorkflowId = (state: { videoState:
+    { selectedWorkflowId: video["selectedWorkflowId"]; }; }) =>
+    state.videoState.selectedWorkflowId
 export const hasChanges = (state: { videoState: { hasChanges: video["hasChanges"]; }; }) =>
   state.videoState.hasChanges
 export const selectWaveformImages = (state: { videoState: { waveformImages: video["waveformImages"]; }; }) =>
