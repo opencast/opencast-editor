02940b16486393926e5c97342d14307e243dbc18
Lars Kiesow <lkiesow@uos.de>
2022-10-28 18:41:05 +0200

    Icon Configuration for Languages
    This patch allows users to specify icons for languages selectors
    displayed in the subtitle editor. Users have to specify the selectable
    languages anyway and the algorithm used before guessed wrong for popular
    languages like English.
    
    If no icon is specified for a language, the selector will simply omit
    the icon and display just the label.
    
    This fixes #825

--- src/main/SubtitleSelect.tsx
+++ src/main/SubtitleSelect.tsx
@@ -62,11 +62,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
     setCanBeAddedFlavors(tempCanBeAddedFlavors)
   }, [captionTracks, subtitles])
 
-  // TODO: Make this function more robust
-  const parseCountryCode = (parseString: string) => {
-    return parseString.split("+").pop()?.slice(0, 2);
-  }
-
   const subtitleSelectStyle = css({
     display: 'grid',
     gridTemplateColumns: 'repeat(auto-fit, minmax(20em, 1fr))',
@@ -80,10 +75,11 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
     }
 
     for (let subFlavor of displayFlavors) {
+      const icon = ((settings.subtitles || {}).icons || {})[subFlavor.subFlavor];
       buttons.push(
         <SubtitleSelectButton
           title={subFlavor.title}
-          iconIdentifier={parseCountryCode(subFlavor.subFlavor)}
+          icon={icon}
           flavor={subFlavor.subFlavor}
           key={subFlavor.subFlavor}
         />
@@ -106,34 +102,20 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
  */
 const SubtitleSelectButton: React.FC<{
   title: string,
-  iconIdentifier: string | undefined,
+  icon: string | undefined,
   flavor: string,
 }> = ({
   title,
-  iconIdentifier,
+  icon,
   flavor
 }) => {
   const { t } = useTranslation();
   const theme = useSelector(selectTheme)
   const dispatch = useDispatch()
 
-  /**
-   * Quick and dirty function to get a flag unicode character by country code
-   * @param countryCode
-   * @returns
-   */
-  function getFlagEmoji(countryCode: string) {
-    var flag = countryCode.toUpperCase().replace(/./g, char =>
-      String.fromCodePoint(127397 + char.charCodeAt(0))
-    );
-    const regexEscape = /[\u{1F1E6}-\u{1F1FF}]/u;
-    if (regexEscape.test(flag)) {
-      return flag
-    }
-  }
-
   const flagStyle = css({
     fontSize: '2em',
+    overflow: 'hidden'
   });
 
   const titleStyle = css({
@@ -156,7 +138,7 @@ const SubtitleSelectButton: React.FC<{
         dispatch(setIsDisplayEditView(true))
         dispatch(setSelectedSubtitleFlavor(flavor))
       }}}>
-      {iconIdentifier && getFlagEmoji(iconIdentifier) && <div css={flagStyle}>{getFlagEmoji(iconIdentifier)}</div>}
+      {icon && <div css={flagStyle}>{icon}</div>}
       <div css={titleStyle}>{title}</div>
     </div>
   );
