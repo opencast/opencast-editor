78adbda8489594ffa8349e109cb4e1ab0f37b85f
Lars Kiesow <lkiesow@uos.de>
2023-04-10 21:23:00 +0200

    Make Studio and Editor look alike
    Opencast Studio and the Opencast Editor are some of the most prominent
    new tools of Opencast. It would be nice if the look and feel of both
    would be more similar so that it feels like they belong to the same
    service.
    
    This patch is a first step. It introduces a header and logo similar to
    Studio.
    
    In the future, we can also use this to give users easy access to
    language options and other settings like with Studio. But that is
    something for future patches.

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,26 @@ import { GlobalStyle } from './cssStyles';
 import { useSelector } from 'react-redux';
 import { selectThemeState } from './redux/themeSlice';
 
+import { css } from '@emotion/react'
+
 function App() {
   const theme = useSelector(selectThemeState);
-  
+
+  const header = css({
+    height: '48px',
+    backgroundColor: '#333333',
+  });
+
+  const logo = css({
+    height: '48px',
+  })
+
   return (
     <div className="App" css={theme}>
       <GlobalStyle />
+      <div css={header}>
+        <img src="opencast-editor.svg" css={logo} alt="Opencast Editor" />
+      </div>
       <Body />
     </div>
   );
