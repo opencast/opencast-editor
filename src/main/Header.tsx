823cb9b75e0900631adccd7baabefaa982c4d8f2
Arnei <arnewilken@yahoo.de>
2023-06-09 15:12:39 +0200

    Redesign - Header Style
    Styles header according to redesgin

--- src/main/Header.tsx
+++ src/main/Header.tsx
@@ -7,23 +7,30 @@ import { css } from '@emotion/react'
 function Header() {
   const themeState = useSelector(selectThemeState);
 
-  const header = themeState.startsWith('high-contrast-')
+  const headerStyle = css({
+    display: 'flex',
+    alignItems: 'center',
+  })
+
+  const headerStyleThemed = themeState.startsWith('high-contrast-')
   ? css({
-    height: '46px',
+    height: '58px',
     backgroundColor: '#000',
     borderBottom: '2px solid white'
   })
   : css({
-    height: '48px',
-    backgroundColor: '#333333',
+
+    height: '60px',
+    backgroundColor: '#4b5563',
   })
 
   const logo = css({
     height: '48px',
+    marginLeft: '6px',
   })
 
   return (
-    <div css={header}>
+    <div css={[headerStyle, headerStyleThemed]}>
       <img src="opencast-editor.svg" css={logo} alt="Opencast Editor" />
     </div>
   );
