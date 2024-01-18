ca513d581d7d9e2ea852941b5d1ea8d694b06a50
Arnei <arnewilken@yahoo.de>
2024-01-18 15:34:10 +0100

    Remove already enforced eslint exceptions
    Removes some eslint exceptions for rules we are
    already (almost) following anyway.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,11 @@
 module.exports = {
   extends: [
     "@opencast/eslint-config-ts-react",
-    "react-app/jest"
+    "react-app/jest",
   ],
   rules: {
-    // "always" gives 319 warning, "never" gives 991.
-    "object-curly-spacing": "off",
-
     // Currently 120 warnings.
     "@typescript-eslint/no-explicit-any": "off",
-
-    // `else` in new line is used often, and often an `if` condition is on the
-    // same line as `{`.
-    "brace-style": "off",
-
-    // Until we figure out how to get to React 18
-    "react/no-deprecated": "off",
   },
   overrides: [
     {
