29aa8b474c80d7141cc603d5bfd4ad05b168009d
Arnei <arnewilken@yahoo.de>
2024-06-27 13:01:08 +0200

    Make use of error boxes from appkit
    The error boxes are small notification boxes that
    inform the user if part of the editor encountered
    a problem that does not completely crash the
    application. This replaces our boxes with the ones
    from appkit, which look way better.

--- src/main/Metadata.tsx
+++ src/main/Metadata.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 import { css } from "@emotion/react";
-import { calendarStyle, errorBoxStyle, selectFieldStyle, titleStyle, titleStyleBold } from "../cssStyles";
+import { calendarStyle, selectFieldStyle, titleStyle, titleStyleBold } from "../cssStyles";
 
 import { useAppDispatch, useAppSelector } from "../redux/store";
 import {
@@ -30,6 +30,7 @@ import { useTheme } from "../themes";
 import { ThemeProvider } from "@mui/material/styles";
 import { cloneDeep } from "lodash";
 import { ParseKeys } from "i18next";
+import { ErrorBox } from "@opencast/appkit";
 
 /**
  * Creates a Metadata form
@@ -710,10 +711,16 @@ const Metadata: React.FC = () => {
             form.reset();
           }} css={metadataStyle}>
 
-            <div css={errorBoxStyle(getStatus === "failed", theme)} role="alert">
-              <span>A problem occurred during communication with Opencast.</span><br />
-              {getError ? "Details: " + getError : "No error details are available."}<br />
-            </div>
+            {getStatus === "failed" &&
+              <ErrorBox>
+                <span css={{ whiteSpace: "pre-line" }}>
+                  {"A problem occurred during communication with Opencast. \n"}
+                  {getError ?
+                    t("various.error-details-text", { errorMessage: getError }) : undefined
+                  }
+                </span>
+              </ErrorBox>
+            }
 
             {catalogs.map((catalog, i) => {
               if (settings.metadata.configureFields) {
@@ -754,11 +761,17 @@ const Metadata: React.FC = () => {
                 </button>
               </div> */}
 
-            <div css={errorBoxStyle(postStatus === "failed", theme)} role="alert">
-              <span>A problem occurred during communication with Opencast. <br />
-                Changes could not be saved to Opencast.</span><br />
-              {postError ? "Details: " + postError : "No error details are available."}<br />
-            </div>
+            {postStatus === "failed" &&
+              <ErrorBox>
+                <span css={{ whiteSpace: "pre-line" }}>
+                  {"A problem occurred during communication with Opencast. \n"
+                  + "Changes could not be saved to Opencast." + "\n"}
+                  {postError ?
+                    t("various.error-details-text", { errorMessage: postError }) : undefined
+                  }
+                </span>
+              </ErrorBox>
+            }
 
             {/* For debugging the forms current values*/}
             {/* <FormSpy subscription={{ values: true }}>
