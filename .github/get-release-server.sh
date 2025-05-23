12dda0ea61e2a6049a046b6cafd2c0f1bd463116
Greg Logan <gregorydlogan@gmail.com>
2025-05-23 17:43:53 -0600

    Correctly sorting the branches

--- .github/get-release-server.sh
+++ .github/get-release-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -uxe
-
 if [ $# -ne 1 ]; then
   echo "Usage: $0 OC_VERSION"
   echo " eg: $0 r/16.x -> Returns the current correct server for r/16.x"
@@ -11,12 +9,12 @@ fi
 git clone https://github.com/opencast/opencast.git ~/opencast
 cd ~/opencast
 
-git branch -a
-
 #Get the list of *all* branches in the format remotes/origin/r/N.m
 #grep for r/N.x
 #then use cut to remove remotes/origin
-ary=( `git branch -a | grep 'r/[0-9]*.x' | sort -nr | head -n 3 | cut -f 3- -d '/'` )
+#then use sort, with a field delimiter of '/', sorting on the *second* key in 'n'umeric 'r'evers order
+#then only consider the first 3 entries
+ary=( `git branch -a | grep 'r/[0-9]*.x' | cut -f 3- d- '/' | sort -t '/' -k 2nr | head -n 3 )
 
 #Iterate through the array above.
 #If the script input matches the first item, spit out develop
