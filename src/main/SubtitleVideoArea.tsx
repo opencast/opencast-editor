0bbc0fa2ed33ce861e9b8cf116c36dd1b3326204
Arnei <arnewilken@yahoo.de>
2026-09-08 11:48:29 +0200

    Remove preview mode TODO in SubtitleVideoArea
    This commit was largely AI generated.

--- src/main/SubtitleVideoArea.tsx
+++ src/main/SubtitleVideoArea.tsx
@@ -129,7 +129,6 @@ const SubtitleVideoArea: React.FC<{
             changeFlavorcallback={setSelectedFlavor}
             defaultFlavor={selectedFlavor}
           />}
-          {/* TODO: Make preview mode work or remove it */}
           <VideoPlayer
             dataKey={0}
             url={getTrackURI()}
